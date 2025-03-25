76a23f5773117721bf2db1296113811d55800897
Laurent Cordier <laurent.cordier28@orange.fr>
2025-03-25 18:05:23 +0100

    Database configuration using .env and profiles

--- src/main/java/com/nnk/springboot/Application.java
+++ src/main/java/com/nnk/springboot/Application.java
@@ -1,12 +1,14 @@
 package com.nnk.springboot;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@Log4j2
+	public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		public static void main(String[] args){
+			SpringApplication.run(Application.class, args);
+		}
 	}
-}
